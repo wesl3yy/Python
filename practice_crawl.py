0408e1a05e0f3577e46198b6aeaf90cbfe9d505f
NamBui <nam.buihai@amela.vn>
2022-09-14 08:37:25 +0700

    Adjust code

--- practice_crawl.py
+++ practice_crawl.py
@@ -1,64 +1,46 @@
-from genericpath import exists
 from selenium import webdriver
 import json
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
-hotel_ = []
+
+hotel = []
+json_data = {}
 count = 0
 driver = webdriver.Firefox(executable_path="geckodriver.exe")
 driver.get("https://travel.rakuten.co.jp/yado/hokkaido/A.html")
+# driver.get("https://search.travel.rakuten.co.jp/ds/yado/hokkaido/A-p3")
 driver.implicitly_wait(5)
 driver.maximize_window()
-title = [
-    title_element.text
-    for title_element in driver.find_elements(By.CSS_SELECTOR, "h1 > a")
-]
-price = [
-    price_element.text
-    for price_element in driver.find_elements(
-        By.CSS_SELECTOR, "p.htlPrice > span:nth-child(1)"
-    )
-]
-url = [
-    url_element.get_attribute("href")
-    for url_element in driver.find_elements(By.CSS_SELECTOR, "h1 > a")
-]
-
-while (
-    bool(
-        WebDriverWait(driver, 5)
-        .until(
-            EC.element_to_be_clickable(
-                (By.CSS_SELECTOR, "div.paging > ul.pagingNumber > li.pagingBack > a")
-            )
-        )
-        .click()
-    )
-    == False
-):
-    page = (
-        WebDriverWait(driver, 5)
-        .until(
-            EC.element_to_be_clickable(
-                (By.CSS_SELECTOR, "div.paging > ul.pagingNumber > li.pagingBack > a")
-            )
-        )
-        .click()
-    )
-    for i in range(len(title)) and range(len(url)) and range(len(price)):
-        json_data = {}
-        count += 1
-        json_data["title"] = title[i]
-        json_data["price"] = price[i]
-        json_data["url"] = url[i]
-        json_data["index"] = count
-        hotel_.append(json_data)
-    print(bool)
-    jsonString = json.dumps(hotel_, indent=1, separators=(",", ":"))
-    jsonFile = open("data.json", "w+")
-    jsonFile.write(jsonString)
-    jsonFile.close()
-driver.close()
+try:
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "ul.htlList")))
+    while driver.current_url:
+        title = [title_element.text for title_element in driver.find_elements(By.CSS_SELECTOR, "div.info > h1 > a")]
+        price = [price_element.text for price_element in driver.find_elements(By.CSS_SELECTOR, "div.info > p.htlPrice > span:nth-child(1)")]
+        url = [url_element.get_attribute("href") for url_element in driver.find_elements(By.CSS_SELECTOR, "div.info > h1 > a")]
+        # if driver.find_elements(By.CSS_SELECTOR, "div.paging > ul.pagingNumber > li.pagingTo > span.pagingActive"):
+        for i in range(len(title)):
+            json_data = {}
+            count += 1
+            try:
+                driver.find_elements(By.CSS_SELECTOR, "div.info > p.htlPrice > span:nth-child(1)")
+                json_data["index"] = count
+                json_data["title"] = title[i]
+                json_data["url"] = url[i]
+                json_data["price"] = price[i]
+            except:
+                json_data["index"] = count
+                json_data["title"] = title[i]
+                json_data["url"] = url[i]
+                json_data["price"] = None
+            hotel.append(json_data)
+        jsonString = json.dumps(hotel, indent=1, separators=(",", ":"))
+        jsonFile = open("data.json", "w+")
+        jsonFile.write(jsonString)
+        jsonFile.close()
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.paging > ul.pagingNumber > li.pagingBack > a"))).click()
+except TimeoutException as e:
+    print(e)
+    driver.close()
